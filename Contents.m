89755396bb7f8dd17ae071111c7df3a7dfa61d02
Pablo Nanez <pananez@gmail.com>
2017-06-01 16:10:34 -0400

    Commit version 2.04 as is in the installer

---
+++ Contents.m
@@ -0,0 +1,14 @@
+% HyEQ_Toolbox_V204
+% Version 2 (04) 17-May-2017
+%
+%    hyeqsolver      -  solves hybrid equations.
+%    plotharc        -    Hybrid arc plot (n states).
+%    plotharccolor   -    Hybrid a 1D arc plot with color.
+%    plotharccolor3d -    Hybrid arc (3D) plot in 3 dimensions with color.
+%    plothybridarc   -    Hybrid arc plot (n states).
+%    plotflows       -   plot a Hybrid arc plot (1D).
+%    plotjumps       -    plot a Hybrid arc plot (1D).
+%    tbclean         -    Removes HyEQ_Toolbox_V202.
+
+% Generated at 23-Apr-2014 18:10:04 by MAKEINSTALL
+% Modified at 17-May-2017 11:18:43 by MAKEINSTALL
